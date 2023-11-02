14c3aef631c1d4655a2b7047ada9ef17a0879a25
Darcy Wong <darcy_wong@sil.org>
2023-11-02 12:02:11 +0700

    feat: Parse XMP tag and update licensing info

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# imageMeta
+# Metadata Utils
+
+Retrieves/updates XMP metadata from audio/image files relating to licensing.
+Uses [exiftools](https://exiftools.org)
+
+* Creator - Name / Organization
+* License - Preferably to set to [Creative Commons](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+* Rights (copyright holder)
+
+## Parameters
+
+**Required** - one of:
+
+-f --files [list of files, separated by commas]
+
+-p --projectPath [path to Fieldworks project]
+
+Metadata-utils will then find all the audio/image files given and update tags according to optional parameters below
+
+**Optional**
+
+-t --tags \<string of JSON Object of metadata tags to write>  
+
+Recommended example of JSON string:
+```
+{\"creator\": \"<Creator of the file>\", \"license\": \"https://creativecommons.org/licenses/by-nc-sa/4.0/\", \"rights\": \"(c) <year> <rights holder>\"}"
+```
+
+-j --json [path to JSON file containg tags to write]
+
+Recommended example of JSON content:
+```JSON
+{
+  "creator": "<Creator of the file>"
+  "license": "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+  "rights": "(c) <year> <Rights holder>"
+}
+```
+
+If a parameter for metadata tags (`--tags` or `--json`) are specified, they'll be written to the file(s)/project.
+Use an empty string to overwrite an existing field.
+
+If none of the optional parameters are given, the metadata tags are just read for the files.
+
+## Pre-requisite
+Install the current LTS of [nodejs](https://nodejs.org/).
---
+++ src/meta.ts
@@ -0,0 +1,133 @@
+import chalk from 'chalk';
+import { exiftool, parseJSON, Tags, WriteTags } from 'exiftool-vendored';
+import path from 'path';
+
+// Utilities to read and write metadata tags from an image file.
+// Currently, we are interested in {creator, license, rights}
+
+export default {getTags, writeImageTags};
+
+export type fileType =
+  "audio" | "image";
+
+// Default creative commons license
+export const DEFAULT_LICENSE_CC_BY_NC_SA = "https://creativecommons.org/licenses/by-nc-sa/4.0/";
+
+export interface linkedFileType {
+  fileName: string;
+  fileType: fileType;
+
+  // primary fields of the tags we care about
+  sourceFile: string | undefined;
+  creator: string | undefined;
+  license: string | undefined;
+  rights: string | undefined;
+
+  // All the tags
+  tags?: Tags;
+}
+
+// The fields of the XMP metadata tag we care about
+export interface xmpTagType {
+  'XMP:Creator'? : string;
+  'XMP:License'? : string;
+  'XMP:Rights'? : string;
+}
+
+chalk.level = 1; // Use colors in the VS Code Debug Window
+
+/**
+ * Retrieve the metadata tags from a list of files
+ * @param files {string[]} - Path to image files
+ * @returns - Map of file id to linkedFileType
+ */
+export async function getTags(files: string[]) : Promise<Map<string, linkedFileType>> {
+  const promises : Array<Promise<any>> = [];
+  files.forEach((file) => {
+    promises.push(exiftool.readRaw(file, ['all', '-xmp:all']));
+  });
+
+  let linkedFiles = new Map();
+  const allTags = await Promise.all(promises);
+  allTags.forEach(rawTags => {
+    const str: string = JSON.stringify(rawTags);
+    const tags: any = parseJSON(str) as any;
+    const id = path.basename(tags.SourceFile);
+    linkedFiles.set(id, {
+      fileName: id,
+      fileType: "image",
+
+      sourceFile: tags.SourceFile,
+      creator: tags.Creator,
+      license: tags.License,
+      rights: tags.Rights,
+
+      // Uncomment if we want more metadata tags
+      //tags: tags
+    });
+  });
+
+  return linkedFiles;
+}
+
+
+
+/**
+ * Get the current metadata tags from a list of image files. Then update
+ * the tags we care about
+ * @param files {string[]} - path to image files
+ * @param newTags {Object} - JSON Object of metadata tags
+ */
+export async function writeImageTags(files: string[], newTags: any) {
+  const fileTags = await getTags(files);
+  const promises : Array<Promise<any>> = [];
+
+  fileTags.forEach((currentTags, id) => {
+    const tagToWrite : xmpTagType = {};
+    // Update creator info
+    if (newTags.hasOwnProperty('creator')) {
+      if (currentTags.creator != newTags.creator) {
+        const warning = `WARNING: Overwriting ${path.basename(currentTags.fileName)} ` +
+          `existing creator: ${currentTags.creator} with ${newTags.creator}`;
+        console.log(chalk.red(warning));
+      }
+
+      tagToWrite['XMP:Creator'] = newTags.creator;
+    }
+
+    // Update License info
+    if (newTags.hasOwnProperty('license')) {
+      if (currentTags.license != newTags.license) {
+        const warning = `WARNING: Overwriting ${path.basename(currentTags.fileName)} ` +
+          `existing license: ${currentTags.license} with ${newTags.license}`;
+        console.log(chalk.red(warning));
+      }
+
+      tagToWrite['XMP:License'] = newTags.license;
+    }
+
+    // Update Rights info
+    if (newTags.hasOwnProperty('rights')) {
+      if (currentTags.rights != newTags.rights) {
+        const warning = `WARNING: Overwriting ${path.basename(currentTags.fileName)} ` +
+          `existing rights: ${currentTags.rights} with ${newTags.rights}`;
+        console.log(chalk.red(warning));
+      }
+
+      tagToWrite['XMP:Rights'] = newTags.rights;
+    }
+
+    // If tagToWrite has content, add it to the promises to write
+    if (Object.keys(tagToWrite).length != 0) {
+      promises.push(
+        exiftool.write(currentTags.sourceFile as string, //files[index],
+          tagToWrite as WriteTags,
+          ['-overwrite_original']));
+    }
+  });
+
+  await Promise.all(promises)
+   .catch((error) => {
+    console.error(error.message);
+   });
+}
